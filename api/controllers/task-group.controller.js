65a05ee71c9e42bac459d65313998fdf8a9b529c
Carlos del Prado <carlos.delpradomota@telefonica.com>
2023-10-05 20:32:38 +0100

    feat: task groups crud

--- api/controllers/task-group.controller.js
+++ api/controllers/task-group.controller.js
@@ -1,7 +1,14 @@
+const createError = require('http-errors');
 const TaskGroup = require('../models/task-group.model');
 
 module.exports.list = (req, res, next) => {
-  TaskGroup.find()
+  const { name } = req.query;
+  const criterial = {};
+  if (name) {
+    criterial.name = name;
+  }
+  
+  TaskGroup.find(criterial)
     .then((group) => res.json(group))
     .catch((error) => next(error));
 }
@@ -9,5 +16,29 @@ module.exports.list = (req, res, next) => {
 module.exports.create = (req, res, next) => {
   TaskGroup.create(req.body)
     .then((group) => res.status(201).json(group))
+    .catch((error) => next(error));
+}
+
+module.exports.delete = (req, res, next) => {
+  TaskGroup.findByIdAndDelete(req.params.id)
+    .then((group) => {
+      if (!group) {
+        next(createError(404, 'TaskGroup not found'));
+      } else {
+        res.status(204).send()
+      }
+    })
+    .catch((error) => next(error));
+}
+
+module.exports.detail = (req, res, next) => {
+  TaskGroup.findById(req.params.id)
+    .then((group) => {
+      if (!group) {
+        next(createError(404, 'TaskGroup not found'));
+      } else {
+        res.json(group);
+      }
+    })
     .catch((error) => next(error));
 }
